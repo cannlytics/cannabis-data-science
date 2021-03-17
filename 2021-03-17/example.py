bf218ca29914d519c79347394e33f6e03ead4b0f
Keegan Skeate <keeganskeate@gmail.com>
2021-03-17 12:27:54 -0700

    Code done during presentation

--- 2021-03-17/example.py
+++ 2021-03-17/example.py
@@ -35,12 +35,12 @@ data = pd.read_excel(
 #-----------------------------------------------------------------------------
 
 # Natural logs.
-# ln_y_t = np.log(data.retail_revenue)
-# ln_k_t = np.log(data.retail_plants)
+ln_y_t = np.log(data.retail_revenue)
+ln_k_t = np.log(data.retail_plants)
 
 # Simple capital only production function.
-# model = sm.OLS(ln_y_t, sm.add_constant(ln_k_t)).fit()
-# print(model.summary())
+model = sm.OLS(ln_y_t, sm.add_constant(ln_k_t)).fit()
+print(model.summary())
 
 
 #-----------------------------------------------------------------------------
@@ -48,22 +48,22 @@ data = pd.read_excel(
 #-----------------------------------------------------------------------------
 
 # Estimate the real rate of return of capital.
-# alpha = model.params.retail_plants
-# r = alpha * data.retail_plants ** (alpha - 1) * 100
+alpha = model.params.retail_plants
+r = alpha * data.retail_plants ** (alpha - 1) * 100 # As a percentage
 
 # Calculate confidence intervals.
-# confidence_interval = model.conf_int()
-# lower_alpha = confidence_interval[0].retail_plants
-# upper_alpha = confidence_interval[1].retail_plants
+confidence_interval = model.conf_int()
+lower_alpha = confidence_interval[0].retail_plants
+upper_alpha = confidence_interval[1].retail_plants
 
 # Estimate upper and lower bounds for real rate of return of capital.
-# lower_r = lower_alpha * data.retail_plants ** (lower_alpha - 1) * 100
-# upper_r = upper_alpha * data.retail_plants ** (upper_alpha - 1) * 100
+lower_r = lower_alpha * data.retail_plants ** (lower_alpha - 1) * 100
+upper_r = upper_alpha * data.retail_plants ** (upper_alpha - 1) * 100
 
 # Simple plot.
-# r.plot()
-# lower_r.plot()
-# upper_r.plot()
+r.plot()
+lower_r.plot()
+upper_r.plot()
 
 
 #-----------------------------------------------------------------------------

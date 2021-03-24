604a6f6b0d285f66cd109d75597d66e4ab4d67e3
Keegan Skeate <keeganskeate@gmail.com>
2021-03-24 09:16:02 -0700

    HHI calculation

--- 2021-03-24/example.py
+++ 2021-03-24/example.py
@@ -13,39 +13,58 @@ Description:
     cannabis market over time in 2020.
 
 Resources:
-    https://sbg.colorado.gov/med-updates
+    https://www.justice.gov/atr/herfindahl-hirschman-index
+    https://data.openthc.org/b2b
 """
-import numpy as np
 import pandas as pd
-import statsmodels.api as sm
 
 #-----------------------------------------------------------------------------
-# Reading in data.
-#-----------------------------------------------------------------------------
-
 # Import the data.
+#-----------------------------------------------------------------------------
+
 data = pd.read_excel(
     './data/wholesale_sales.xlsx',
-     sheet_name='CO Monthly Data',
+     sheet_name='Panel',
      col=0
 )
-
 
 #-----------------------------------------------------------------------------
 # Calculate total sales by month.
 #-----------------------------------------------------------------------------
 
+sales_by_month = {}
+months = data.month.unique()
+for month in months:
+    total_sales = data.loc[data.month == month].sales.sum()
+    sales_by_month[month] = total_sales
 
 #-----------------------------------------------------------------------------
-# Calculate market share for each wholesaler by month.
+# Calculate market share for each wholesaler by month. (Refactor!)
 #-----------------------------------------------------------------------------
 
+market_shares = []
+for index, values in data.iterrows():
+    market_share = values.sales / sales_by_month[values.month]
+    market_shares.append(market_share)
+data['market_share'] = pd.Series(market_shares)
+    
 
 #-----------------------------------------------------------------------------
 # Calculate the HHI by month.
 #-----------------------------------------------------------------------------
 
+hhi_by_month = {}
+for month in months:
+    month_data = data.loc[data.month == month]
+    hhi = 0
+    for index, values in month_data.iterrows():
+        hhi += values.market_share ** 2
+    hhi_by_month[month] = hhi
 
 #-----------------------------------------------------------------------------
 # Plot the HHI
 #-----------------------------------------------------------------------------
+
+hhi_data = pd.Series(hhi_by_month)
+hhi_data = hhi_data.sort_index()
+hhi_data.plot()

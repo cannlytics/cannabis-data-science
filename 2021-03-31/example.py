818b1cb3fd7ac58568c5f6e0a58e8e631d9e65f4
Keegan Skeate <keeganskeate@gmail.com>
2021-03-31 09:42:21 -0700

    Code during meetup 👋

--- 2021-03-31/example.py
+++ 2021-03-31/example.py
@@ -15,6 +15,8 @@ Description:
 
 Resources:
     
+    https://stackoverflow.com/questions/59882714/python-generating-a-list-of-dates-between-two-dates/59882807
+
 """
 
 # External imports
@@ -31,6 +33,11 @@ data = pd.read_excel(
     './data/co_report_data.xlsx',
      col=0
 )
+data.index = pd.date_range(
+    '2014-01-01',
+   '2020-07-01',
+    freq='m'
+)
 
 
 #-----------------------------------------------------------------------------
@@ -38,10 +45,32 @@ data = pd.read_excel(
 #-----------------------------------------------------------------------------
 
 total_revenue_forecast = arima_min_rmse_forecast(
-    data.total_revenue.values,
-    lag_order=6,
+    data.total_plants.values,
+    lag_order=3,
     hold_out_period=6,
     forecast_steps=18,
     verbose=True
 )
+total_revenue_forecast = pd.Series(total_revenue_forecast)
+total_revenue_forecast.index = pd.date_range(
+    '2020-07-01',
+   '2022-01-01',
+    freq='m'
+)
+
+
+#-----------------------------------------------------------------------------
+# ARMA estimation.
+#-----------------------------------------------------------------------------
+
+# from statsmodels.tsa.arima_model import ARIMA
+# model = ARIMA(data.total_revenue.values, order=(4, 0, 6))
+# model_fit = model.fit(disp=0)
+
+#-----------------------------------------------------------------------------
+# Plot the data.
+#-----------------------------------------------------------------------------
+
+data.total_revenue.plot()
+total_revenue_forecast.plot()
 

08f7c67489abea87e09eecf45d10c0ca58015037
Keegan Skeate <keeganskeate@gmail.com>
2021-11-10 13:05:46 -0500

    Uploaded (ironed out) code from 11/10/2021 meetup

--- 2021-11-10/predicting_market_performance_pt_2.py
+++ 2021-11-10/predicting_market_performance_pt_2.py
@@ -5,7 +5,7 @@ Copyright (c) 2021 Cannlytics and the Cannabis Data Science Meetup Group
 
 Authors: Keegan Skeate <keegan@cannlytics.com>
 Created: 10/6/2021
-Updated: 11/3/2021
+Updated: 11/10/2021
 License: MIT License <https://opensource.org/licenses/MIT>
 
 References:
@@ -33,13 +33,16 @@ Data Sources:
 from dotenv import dotenv_values
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
+import numpy as np
 import pandas as pd
 import pmdarima as pm
 import requests
 import seaborn as sns
+import statsmodels.api as sm
 
 # Internal imports
 from utils import (
+    end_of_period_timeseries,
     forecast_arima,
     format_millions,
     format_thousands,
@@ -171,9 +174,10 @@ plt.show()
 #--------------------------------------------------------------------------
 # Estimate sales, plants grown, and employees,
 # total retailers, total cultivators, and total licensees
-# in 2021 and 2022 # using Box-Jenkins (ARIMA) methodology.
+# in 2021 and 2022 using Box-Jenkins (ARIMA) methodology.
 # Month fixed effects are used.
-# Optional: Attempt to forecast with daily series with day-of-the-week fixed effects.
+# Optional: Attempt to forecast with daily series with
+# day-of-the-week fixed effects.
 #--------------------------------------------------------------------------
 
 # Specifiy training time periods.
@@ -369,106 +373,162 @@ forecast_plants_per_cultivator =  plants_forecast / total_cultivators_forecast
 forecast_employees_per_license =  employees_forecast / total_licensees_forecast
 
 #--------------------------------------------------------------------------
-# TODO: Visualize the forecasts as 2x3 time series plots.
+# Visualize the forecasts as 2x3 time series plots.
 # Sales | Plants
 # Retailers | Cultivators
 # Sales per Retailer | Plants per Cultivator
 #--------------------------------------------------------------------------
 
+def plot_forecast(
+        ax,
+        forecast,
+        historic=None,
+        conf=None,
+        title=None,
+        color=None,
+        formatter=None,
+):
+    """Plot a time series forecast.
+    Args:
+        ax (): The axes on which to plot the forecast.
+        forecast (Series): The forecast to plot.
+        historic (Series): Optional historic time series to plot.
+        conf (Array): An optional 2xN array of lower and upper confidence
+            bounds for the forecast series.
+        title (str): An optional title to place above the chart.
+        color: (str): An optional color hex code.
+        formatter (func): An optional formatting function for the Y axis.
+    """
+    forecast.plot(color=color, style='--', label='Forecast')
+    if conf is not None:
+        plt.fill_between(
+            forecast.index,
+            conf[:, 0],
+            conf[:, 1],
+            alpha=0.1,
+            color=color,
+        )
+    if historic is not None:
+        historic.plot(color=color, label='Historic')
+    if title is not None:
+        plt.title(title, fontsize=24, pad=10)
+    if formatter is not None:
+        yaxis_format = FuncFormatter(formatter)
+        ax.yaxis.set_major_formatter(yaxis_format)
+    plt.gca().set(ylim=0)
+    plt.setp(ax.get_yticklabels()[0], visible=False)
+    plt.xlabel('')
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+
 # Define the plot style.
 plt.style.use('fivethirtyeight')
 plt.rcParams['font.family'] = 'Times New Roman'
 palette = sns.color_palette('tab10')
-primary_color = palette[0]
-secondary_color = palette[-1]
-
-# TODO: Pick out colors for all series.
-
-# Plot sales forecast.
-# fig, ax = plt.subplots(figsize=(15, 5))
-# weekly_sales.plot(ax=ax, color=primary_color, label='Historic')
-# sales_forecast.plot(ax=ax, color=secondary_color, style='--', label='Forecast')
-# plt.fill_between(
-#     sales_forecast.index,
-#     sales_conf[:, 0],
-#     sales_conf[:, 1],
-#     alpha=0.1,
-#     color=secondary_color,
-# )
-# plt.legend(loc='lower left', fontsize=18)
-# plt.title('Massachusetts Cannabis Sales Forecast', fontsize=24, pad=10)
-# yaxis_format = FuncFormatter(format_millions)
-# ax.yaxis.set_major_formatter(yaxis_format)
-# plt.gca().set(ylim=0)
-# plt.setp(ax.get_yticklabels()[0], visible=False)
-# plt.xlabel('')
-# plt.xticks(fontsize=18)
-# plt.yticks(fontsize=18)
-# plt.show()
-
-# TODO: Reduce boilerplate when creating charts (create a function?).
-series_list = [[weekly_sales, weekly_plants]]
 
 # Plot all series.
-fig = plt.figure(figsize=(25, 5))
+fig = plt.figure(figsize=(40, 25))
 
 # Plot sales.
-ax1 = plt.subplot(1, 2, 1)
-weekly_sales.plot(color=primary_color, label='Historic')
-sales_forecast.plot(color=primary_color, style='--', label='Forecast')
-plt.fill_between(
-    sales_forecast.index,
-    sales_conf[:, 0],
-    sales_conf[:, 1],
-    alpha=0.1,
-    color=primary_color,
+ax1 = plt.subplot(3, 2, 1)
+plot_forecast(
+        ax1,
+        sales_forecast,
+        historic=weekly_sales,
+        conf=sales_conf,
+        title='Cannabis Sales',
+        color=palette[0],
+        formatter=format_millions,
 )
-plt.title('Massachusetts Cannabis Sales Forecast', fontsize=24, pad=10)
-yaxis_format = FuncFormatter(format_millions)
-ax1.yaxis.set_major_formatter(yaxis_format)
-plt.gca().set(ylim=0)
-plt.setp(ax1.get_yticklabels()[0], visible=False)
-plt.xlabel('')
-plt.xticks(fontsize=18)
-plt.yticks(fontsize=18)
 
 # Plot plants.
-ax2 = plt.subplot(1, 2, 2)
-weekly_plants.plot(color=secondary_color, label='Historic Plants')
-plants_forecast.plot(color=secondary_color, style='--', label='Forecast')
-plt.fill_between(
-    plants_forecast.index,
-    plants_conf[:, 0],
-    plants_conf[:, 1],
-    alpha=0.1,
-    color=secondary_color,
+ax2 = plt.subplot(3, 2, 2)
+plot_forecast(
+        ax2,
+        plants_forecast,
+        historic=weekly_plants,
+        conf=plants_conf,
+        title='Cannabis Plants',
+        color=palette[-1],
+        formatter=format_thousands,
 )
-plt.title('Massachusetts Cannabis Plants Forecast', fontsize=24, pad=10)
-yaxis_format = FuncFormatter(format_thousands)
-ax2.yaxis.set_major_formatter(yaxis_format)
-plt.gca().set(ylim=0)
-plt.setp(ax2.get_yticklabels()[0], visible=False)
-plt.xlabel('')
-plt.xticks(fontsize=18)
-plt.yticks(fontsize=18)
 
 # Plot retailers.
+ax3= plt.subplot(3, 2, 3)
+plot_forecast(
+        ax3,
+        total_retailers_forecast,
+        historic=weekly_total_retailers,
+        conf=total_retailers_conf,
+        title='Cannabis Retailers',
+        color=palette[3],
+        # formatter=format_thousands,
+)
 
 # Plot cultivators.
+ax4= plt.subplot(3, 2, 4)
+plot_forecast(
+        ax4,
+        total_cultivators_forecast,
+        historic=weekly_total_cultivators,
+        conf=total_cultivators_conf,
+        title='Cannabis Cultivators',
+        color=palette[2],
+        # formatter=format_thousands,
+)
 
 # Plot average sales per retailer.
+ax5= plt.subplot(3, 2, 5)
+plot_forecast(
+        ax5,
+        forecast_sales_per_retailer,
+        historic=sales_per_retailer,
+        # conf=total_cultivators_conf,
+        title='Average Cannabis Sales per Retailer',
+        color=palette[4],
+        formatter=format_thousands,
+)
 
 # Plot average plants per cultivator.
+ax6 = plt.subplot(3, 2, 6)
+plot_forecast(
+        ax6,
+        forecast_plants_per_cultivator,
+        historic=plants_per_cultivator,
+        # conf=total_cultivators_conf,
+        title='Average Cannabis Plants per Cultivator',
+        color=palette[5],
+        # formatter=format_thousands,
+)
 
+# Add figure notes.
+notes = """Data: Cannabis sales, total tracked plants, and licensees data from October 15, 2018 through October 26, 2021.
+Data Source: MA Cannabis Control Commission."""
+plt.figtext(0.05, -0.01, notes, ha='left', fontsize=16)
 
-# plt.legend(loc='lower left', fontsize=18)
+# Add a title above all of the subplots.
+fig.suptitle(
+    'Massachusetts Historic and Predicted Cannabis Market Performance',
+    fontsize=40
+)
 
+# Save the figure.
+plt.margins(1, 1)
+plt.savefig(
+    'figures/ma_market_forecast.png',
+    dpi=300,
+    bbox_inches='tight',
+    pad_inches=0.75,
+    transparent=False,
+)
+
+# Show the figure.
 plt.show()
 
-# TODO: Find a clever way to display 2022 forecast totals (curly braces?)
+# Optional: Show a legend?
+# plt.legend(loc='lower left', fontsize=18)
 
-
-
+# Optional: Find a clever way to display 2022 forecast totals (curly braces?)
 
 
 #--------------------------------------------------------------------------
@@ -477,60 +537,66 @@ plt.show()
 # getting supplemental data from FRED (Federal Reserve Economic Data).
 #--------------------------------------------------------------------------
 
-# # Initialize Fred client.
-# config = dotenv_values('../.env')
-# fred = Fred(api_key=config.get('FRED_API_KEY'))
+# Initialize Fred client.
+from fredapi import Fred
+config = dotenv_values('../.env')
+fred = Fred(api_key=config.get('FRED_API_KEY'))
 
-# # Find the observation time start.
-# observation_start = production.index.min().isoformat()
+# Find the observation time start.
+observation_start = production.index.min().isoformat()
 
-# # # Optional: Get MA population (conjecturing that population remains constant in 2021).
-# # population = fred.get_series('MAPOP', observation_start=observation_start)
-# # population = end_of_period_timeseries(population, 'Y')
-# # population = population.multiply(1000) # Thousands of people
-# # new_row = pd.DataFrame([population[-1]], index=[pd.to_datetime('2021-12-31')])
-# # population = pd.concat([population, pd.DataFrame(new_row)], ignore_index=False)
-
-# # Get average weekly hours worked in MA.
-# avg_weekly_hours = fred.get_series('SMU25000000500000002SA', observation_start=observation_start)
-# avg_weekly_hours = end_of_period_timeseries(avg_weekly_hours)
-# avg_monthly_hours = avg_weekly_hours.resample('M').sum()
+# Get average weekly hours worked in MA.
+avg_weekly_hours = fred.get_series('SMU25000000500000002SA', observation_start=observation_start)
+avg_weekly_hours = end_of_period_timeseries(avg_weekly_hours)
+avg_weekly_hours = avg_weekly_hours.resample('W-Sun').ffill().iloc[:-1]
 
 #--------------------------------------------------------------------------
 # Optional: Estimate historic competitive wages and interest rates.
 #--------------------------------------------------------------------------
 
-# # Define variables.
-# Y = weekly_sales
-# K = weekly_plants
-# L = weekly_employees * avg_weekly_hours
+# Define variables.
+Y = weekly_sales
+K = weekly_plants
+L = weekly_employees * avg_weekly_hours
 
-# # Exclude missing observations.
-# missing_sales = Y.loc[Y == 0].index
-# Y = Y[~Y.index.isin(missing_sales)]
-# K = K[~K.index.isin(missing_sales)]
-# L = L[~L.index.isin(missing_sales)]
+# Exclude missing observations.
+missing_sales = Y.loc[Y == 0].index
+Y = Y[~Y.index.isin(missing_sales)]
+K = K[~K.index.isin(missing_sales)]
+L = L[~L.index.isin(missing_sales)]
 
-# # Restrict time frame.
-# Y = Y[Y.index >= pd.to_datetime('2020-06-01')]
-# K = K[K.index >= pd.to_datetime('2020-06-01')]
-# L = L[L.index >= pd.to_datetime('2020-06-01')]
+# Restrict time frame.
+Y = Y.loc[(Y.index >= pd.to_datetime('2019-01-01')) & (Y.index <= pd.to_datetime('2021-08-01'))]
+K = K.loc[(K.index >= pd.to_datetime('2019-01-01')) & (K.index <= pd.to_datetime('2021-08-01'))]
+L = L.loc[(L.index >= pd.to_datetime('2019-01-01')) & (L.index <= pd.to_datetime('2021-08-01'))]
 
-# # Define per labor variables.
-# y = Y / L
-# k = K / L
+# Estimate alpha and beta.
+# The typical is 0.3 for alpha and
+# 0.7 for beta for most industries.
+ln_y = np.log(Y)
+ln_k = np.log(K)
+ln_l = np.log(L)
+X = pd.concat([ln_k, ln_l], axis=1)
+X = sm.add_constant(X)
+regression = sm.OLS(ln_y, X).fit()
+print(regression.summary())
+alpha = regression.params['total_planttrackedcount']
+beta = regression.params[0]
 
-# # Estimate alpha.
-# ln_y = np.log(y)
-# ln_k = np.log(k)
-# ln_x = np.asarray(sm.add_constant(ln_k))
-# regression = sm.OLS(ln_y, ln_x).fit()
-# print(regression.summary())
+# Estimate historic competitive wage.
+wage = beta * Y / L
+wage.plot()
+plt.show()
 
+# Estimate historic competitive interest rate.
+interest_rates = alpha * Y / K
+interest_rates.plot()
+plt.show()
 
 #--------------------------------------------------------------------------
 # Optional: Predict future competitive wages and interest rates
 # either using estimated historic production function parameters
 # or by estimating a new production function for the future observations.
 #--------------------------------------------------------------------------
+
 

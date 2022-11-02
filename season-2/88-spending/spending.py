90e5703d69f481f6a346c4ffcf138d6970b48c3b
Keegan Skeate <keeganskeate@gmail.com>
2022-11-02 14:11:56 -0500

    Spend data extraction with OCR 👀 + NLP ✍️

--- season-2/88-spending/spending.py
+++ season-2/88-spending/spending.py
@@ -1,236 +1,175 @@
 """
-Dosage Analysis
+Spending Trending
 Copyright (c) 2022 Cannlytics
 
 Authors:
     Keegan Skeate <https://github.com/keeganskeate>
-Created: 10/10/2022
-Updated: 10/12/2022
+Created: 11/2/2022
+Updated: 11/2/2022
 License: <https://github.com/cannlytics/cannabis-data-science/blob/main/LICENSE>
 """
 # Standard imports.
-from datetime import datetime
 import os
-import random
 
 # External imports.
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-
-# Internal imports.
-from cannlytics.data.coas import CoADoc
+from cannlytics.utils import sorted_nicely
 
 
 #------------------------------------------------------------------------------
-# Parse COA PDFs with CoADoc!
+# Read in all of the receipt images.
 #------------------------------------------------------------------------------
 
-# Specify where your Cannlytics repo lives.
-COA_DIR = '../../assets/coas/ma'
-DATA_DIR = '../../.datasets/coas'
-
-# Initialize CoADoc.
-parser = CoADoc()
-
-# Read COA data from all specified directories.
-coa_data = []
-filenames = os.listdir(COA_DIR)
+# Specify where your receipts live.
+RECEIPT_DIR = '../../.datasets/receipts'
+receipt_filenames = []
+filenames = os.listdir(RECEIPT_DIR)
 for filename in filenames:
-    filename = filename.replace('.PDF', '.pdf')
-    file_path = f'{DATA_DIR}/{filename}'
-    data = parser.parse(file_path)
-    coa_data.append(data[0])
-    print('Parsed:', filename)
-
-# Save the data.
-timestamp = datetime.now().isoformat()[:19].replace(':', '-')
-if not os.path.exists(DATA_DIR): os.makedirs(DATA_DIR)
-outfile = f'{DATA_DIR}/ma-coas-{timestamp}.xlsx'
-parser.save(coa_data, outfile)
-print('Successfully parsed %i COAs!' % len(coa_data))
+    if filename.endswith('.jpg'):
+        receipt_filenames.append(f'{RECEIPT_DIR}/{filename}')
 
 
 #------------------------------------------------------------------------------
-# Visualize the COA data.
+# Parse the text from the images with OCR.
 #------------------------------------------------------------------------------
 
-def scatter_text(ax, x, y, text_column, data, color='#004b09'):
-    """Add text besides each point of a scatterplot.
-       Based on this answer: https://stackoverflow.com/a/54789170/2641825"""
-    for line in range(0, data.shape[0]):
-         ax.text(
-            data[x].iloc[line] + 0.01,
-            data[y].iloc[line], 
-            data[text_column].iloc[line],
-            horizontalalignment='left', 
-            color=color,
-        )
+import cv2
+import pdfplumber
+import pytesseract
+import re
 
 
-# Setup plotting style.
-plt.style.use('fivethirtyeight')
-plt.rcParams.update({
-    'figure.figsize': (12, 8),
-    'font.family': 'Times New Roman',
-    'font.size': 21,
-})
+def rotate(image, center=None, scale=1.0):
+    """Rotate an image with text into a readable position, as well as possible.
+    Credit: Mousam Singh <https://stackoverflow.com/a/55122911/5021266>
+    License: CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0/>
+    """
+    (h, w) = image.shape[:2]
+    if center is None:
+        center = (w / 2, h / 2)
+    degrees = int(re.search(
+        '(?<=Rotate: )\d+',
+        pytesseract.image_to_osd(image),
+    ).group(0))
+    angle = 360 - degrees
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, M, (w, h))
+    return rotated
 
-# Read in the COA data.
-datafile = f'{DATA_DIR}/ma-coas-2022-10-10T15-56-06.xlsx'
-data = pd.read_excel(datafile)
 
-# List the product types.
-product_types = list(data.product_type.unique())
-print('Product types:', product_types)
+def image_to_text(image_file):
+    """Extract the text from an image, rotating it as necessary."""
 
-# Clean product names.
-data['strain_name'] = data['product_name'].apply(
-    lambda x: x.split(')')[-1].split(',,')[0].split('Bulk')[0]
-)
+    # Rotate the image as needed.
+    image_data = rotate(cv2.imread(str(image_file), cv2.IMREAD_COLOR))
 
-# Calculate total THC and total CBD.
-from cannlytics.utils.constants import DECARB
-data['total_thc'] = data['delta_9_thc'] + data['thca'] * DECARB
-data['total_cbd'] = data['cbda'] + data['cbda'] * DECARB
-data['total_cbg'] = data['cbg'] + data['cbga'] * DECARB
+    # Convert the image to PDF.
+    pdf = pytesseract.image_to_pdf_or_hocr(image_data, extension='pdf')
+    pdf_file = image_file.replace('.jpg', '.pdf')
+    with open(pdf_file, 'w+b') as f:
+        f.write(pdf)
+        f.close()
 
-# Calculate log THC to CBG ratio.
-data['thc_to_cbg'] = data['total_thc'] / data['total_cbg']
+    # Extract the text from the PDF.
+    receipt = pdfplumber.open(pdf_file)
+    text = ''
+    for page in receipt.pages:
+        text += page.extract_text()
+    return text
 
-# Calculate beta-pinene to d-limonene ratio.
-data['beta_pinene_to_d_limonene'] = data['beta_pinene'] / data['d_limonene']
 
-# Calculate
-data['alpha_humulene_to_beta_caryophyllene'] = data['alpha_humulene'] / data['beta_caryophyllene']
+# Example: Extract the text from a receipt.
+image_file = '../../.datasets/receipts/receipt-1.jpg'
+image_text = image_to_text(image_file)
 
-# Separate product types.
-joints = data.loc[data['product_type'] == 'Flower']
+# Example: Extract the text from all receipts.
+receipt_texts = []
+for filename in receipt_filenames:
+    image_text = image_to_text(filename)
+    receipt_texts.append(image_text)
 
-# Plot CBD to THC scatterplot with annotations.
-ax = sns.scatterplot(
-    data=joints,
-    y='total_cbg',
-    x='total_thc',
-    hue='thc_to_cbg',
-    palette='viridis_r',
-    s=400,
-)
-scatter_text(
-    ax,
-    y='total_cbg',
-    x='total_thc',
-    text_column='strain_name',
-    data = joints,
-)
-plt.legend(loc='lower right')
-plt.title('CBG to THC ratio of a sample of MA joints')
-plt.show()
 
-# Restrict to those with terpenes.
-terpene_joints = joints.loc[
-    ~joints['beta_pinene_to_d_limonene'].isna()
+#------------------------------------------------------------------------------
+# Extract the key data from the text with NLP.
+#------------------------------------------------------------------------------
+
+import spacy
+from spacy import displacy
+
+
+# Create natural language processing client.
+# Use `en_core_web_sm` for speed and `en_core_web_trf` for accuracy.
+# For a blank model, use spacy.blank('en')
+nlp = spacy.load('en_core_web_lg')
+
+# Look at easily-recognized entities.
+doc = nlp(receipt_texts[0])
+displacy.render(doc, style='ent')
+
+
+#------------------------------------------------------------------------------
+
+# Extract all of the money entities.
+time_entities = []
+money_entities = []
+for i in doc.ents:
+    entry = str(i.lemma_).lower()
+    if i.label_ in ['MONEY']:
+        money_entities.append(entry)
+
+
+#------------------------------------------------------------------------------
+
+# TODO: Identify times.
+
+
+#------------------------------------------------------------------------------
+
+# FIXME: Identify quantities.
+patterns = [
+    {
+        'label': 'QUANTITY',
+        'pattern': [
+            {'LIKE_NUM': True},
+            {'LOWER': {'IN': [
+                'g', 'gr', 'gram', 'grams', 'teenth', 'sixteenth', 'eighth',
+                'quarter', 'ounce', 'ounces', 'oz', 'pound', 'lb', 'mg', 'kg',
+                'milligram', 'milligrams', 'kilogram', 'kilograms',
+                '1\/8 oz'
+            ]}}
+        ],
+    },
 ]
+try:
+    ruler = nlp.add_pipe('entity_ruler', after='ner')
+except ValueError:
+    nlp.remove_pipe('entity_ruler')
+    ruler = nlp.add_pipe('entity_ruler', after='ner')
+ruler.add_patterns(patterns)
 
-# Plot beta-pinene to d-limonene scatterplot with annotations.
-ax = sns.scatterplot(
-    data=terpene_joints,
-    x='d_limonene',
-    y='beta_pinene',
-    hue='beta_pinene_to_d_limonene',
-    palette='viridis',
-    s=400,
-)
-scatter_text(
-    ax,
-    x='d_limonene',
-    y='beta_pinene',
-    text_column='strain_name',
-    data = terpene_joints,
-)
-plt.title('beta-Pinene to D-limonene ratio of a sample of MA flower')
-plt.show()
-
-# Plot alpha-humulene to beta-caryophyllene scatterplot with annotations.
-ax = sns.scatterplot(
-    data=terpene_joints,
-    x='beta_caryophyllene',
-    y='alpha_humulene',
-    hue='alpha_humulene_to_beta_caryophyllene',
-    palette='viridis_r',
-    s=400,
-)
-scatter_text(
-    ax,
-    x='beta_caryophyllene',
-    y='alpha_humulene',
-    text_column='strain_name',
-    data = terpene_joints,
-)
-plt.title('alpha-Humulene to beta-caryophyllene of a sample of MA flower')
-plt.show()
+# Look at the receipts for quantities.
+doc = nlp(receipt_texts[0])
+displacy.render(doc, style='ent')
 
 
 #------------------------------------------------------------------------------
-# Example analysis of the COA data.: Smoke 2 Joints
+
+# TODO: Identify strain names.
+
+
+#------------------------------------------------------------------------------
+# Analyze the data.
 #------------------------------------------------------------------------------
 
-# Read the data.
-random.choice()
+from transformers import GPT2Tokenizer
 
-# Draw 6 joint, 365 times.
-# ASSUMPTION: Each joint 1g.
-consumption = []
-days = range(0, 365)
-for day in days:
-    daily_consumption = terpene_joints.sample(
-        1,
-        replace=True,
-    )
-    consumption.append(daily_consumption)
+# Use a pre-trained model to tokenize the receipts.
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+tokenizer(image_text)['input_ids']
 
-# Plot daily, weekly, monthly consumption.
-daily = pd.DataFrame()
-analytes = ['total_thc', 'total_cbg', 'beta_pinene', 'd_limonene',
-    'beta_caryophyllene', 'alpha_humulene']
-colors = sns.color_palette('tab10', n_colors=len(analytes))
-for analyte in analytes:
-    daily[analyte] = pd.Series(
-        [x[analyte].sum() * 10 for x in consumption],
-        name=analyte,
-    )
 
-# Visualize daily consumption.
-for i, analyte in enumerate(analytes):
-    daily[analyte].plot(color=colors[i])
-    name = analyte.replace('_', ' ').title()
-    plt.title(f'Daily {name} consumption (mg)')
-    plt.show()
+#------------------------------------------------------------------------------
+# Trend the spending!
+#------------------------------------------------------------------------------
 
-# Visualize weekly consumption.
-for i, analyte in enumerate(analytes):
-    daily[analyte].rolling(7).sum().plot(color=colors[i])
-    name = analyte.replace('_', ' ').title()
-    plt.title(f'Weekly {name} consumption (mg)')
-    plt.show()
+# Future work: Trend the spending amount by month!
 
-# Visualize monthly consumption.
-for i, analyte in enumerate(analytes):
-    daily[analyte].rolling(30).sum().plot(color=colors[i])
-    name = analyte.replace('_', ' ').title()
-    plt.title(f'Monthly {name} consumption (mg)')
-    plt.show()
-
-# Calculate annual consumption.
-for analyte in analytes:
-    average_daily = daily[analyte].mean()
-    average_weekly = daily[analyte].rolling(7).sum().mean()
-    average_monthly = daily[analyte].rolling(30).sum().mean()
-    annual = daily[analyte].sum()
-    name = analyte.replace('_', ' ').title()
-    print(f'\nConsumption of {name}')
-    print('----------------------')
-    print('Avg. Daily (mg):', round(average_daily, 2))
-    print('Avg. Weekly (mg):', round(average_weekly, 2))
-    print('Avg. Monthly (mg):', round(average_monthly, 0))
-    print('Annual (g):', round(annual / 1000, 0))
